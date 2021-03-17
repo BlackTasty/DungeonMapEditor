02f631d83123f7ef64dccc6a7590454f196e183d
Raphael Winkler <raphael84@live.at>
2021-03-17 03:49:02 +0100

    Added style for tooltips, new button style

---
+++ DungeonMapEditor/Converter/DoubleDivideByConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DungeonMapEditor.Converter
+{
+    class DoubleDivideByConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue && parameter is string dividerRaw && double.TryParse(dividerRaw, out double divider))
+            {
+                return doubleValue / divider;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
